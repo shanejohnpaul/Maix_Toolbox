7e4486f59320218bfea4f4fc551d05d4f66e7a4e
L.Shane John Paul <shanejohnpaul1998@gmail.com>
2020-09-24 13:03:06 +0530

    Update get_nncase.sh
    Change typo - .gz to .xz

--- get_nncase.sh
+++ get_nncase.sh
@@ -5,6 +5,6 @@ mkdir -p images
 mkdir -p log
 cd ncc
 wget https://github.com/kendryte/nncase/releases/download/v0.1.0-rc5/ncc-linux-x86_64.tar.xz
-tar -Jxf ncc-linux-x86_64.tar.gz
-rm ncc-linux-x86_64.tar.gz
+tar -Jxf ncc-linux-x86_64.tar.xz
+rm ncc-linux-x86_64.tar.xz
 echo "download nncase ok!"
